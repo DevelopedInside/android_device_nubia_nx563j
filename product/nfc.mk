81411d945f6e4c8b341ae62f00d05d201575d5c7
BeYkeRYkt <beykerykt@gmail.com>
2017-11-19 19:43:57 +0900

    nx563j: build nfc_nci.bcm2079x
    Change-Id: I2a5a832d5d4b1e875ec31cafff2c55d78602ef81

--- product/nfc.mk
+++ product/nfc.mk
@@ -1,5 +1,6 @@
 # NFC
 PRODUCT_PACKAGES += \
+    nfc_nci.bcm2079x.default \
     com.android.nfc_extras \
     NQNfcNci \
     Tag
@@ -14,5 +15,4 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
+    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
