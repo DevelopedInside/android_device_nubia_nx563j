cb7cea8fe7f3d6d479149ad7377833df80d6e6bf
BeYkeRYkt <beykerykt@gmail.com>
2017-11-26 23:20:48 +0900

    nx563j: introduce setup-makefiles.sh
    * i'm missed him
    
    Change-Id: I7afc9957fc6865b166d5c938e34760f49a8347d9

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 #
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +13,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-LOCAL_PATH := $(call my-dir)
+set -e
 
-include $(call first-makefiles-under,$(LOCAL_PATH))
+# Required!
+export DEVICE=nx563j
+export DEVICE_COMMON=msm8998-common
+export VENDOR=nubia
+
+export DEVICE_BRINGUP_YEAR=2017
+
+./../../$VENDOR/$DEVICE_COMMON/setup-makefiles.sh $@
