cea2a976caf609c7a583b042f3a3fb504e5b599e
Andre Eisenbach <eisenbach@google.com>
2017-12-06 16:16:36 -0800

    nx563j: Rename libnfc-brcm.conf -> libnfc-nci.conf
    Test: manual
    [mikeioannina]: Keep old config for vendor HAL
    Merged-In: I9655a14e021024540cf342022cb817e1f8d0f9ad
    Change-Id: I9655a14e021024540cf342022cb817e1f8d0f9ad

--- device.mk
+++ device.mk
@@ -86,7 +86,8 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf
+    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
 
 # Properties
 -include $(LOCAL_PATH)/vendor_prop.mk
