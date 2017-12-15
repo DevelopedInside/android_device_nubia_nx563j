6bee7a3839c31471598c48a841aa0a14cb41b791
BeYkeRYkt <beykerykt@gmail.com>
2017-12-15 22:07:14 +0900

    nx563j: move Dalvik/HWUI to common
    Change-Id: Ib6ac706a13a69a7840dc6efe8e26c6646823b455

--- device.mk
+++ device.mk
@@ -38,29 +38,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/display/calib.cfg:system/etc/calib.cfg \
     $(LOCAL_PATH)/display/qdcm_calib_data_jdi_r63452_1080p_5p5_mipi_cmd_panel.xml:system/etc/qdcm_calib_data_jdi_r63452_1080p_5p5_mipi_cmd_panel.xml
 
-# Dalvik from Stock
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m
-
-# HWUI from Stock
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=72 \
-    ro.hwui.layer_cache_size=48 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.path_cache_size=32 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024
-
 # Init scripts
 PRODUCT_PACKAGES += \
     init.target.rc
