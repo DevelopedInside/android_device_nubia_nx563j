b38a560166232e8c7304160542f422e354d54e28
BeYkeRYkt <beykerykt@gmail.com>
2019-06-11 19:57:13 +0900

    nx563j: touch: it's nx563j
    Change-Id: Iafa416591751946661ee099c99a72d91f1b15883

--- touch/service.cpp
+++ touch/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "vendor.lineage.touch@1.0-service.sagit"
+#define LOG_TAG "vendor.lineage.touch@1.0-service.nx563j"
 
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
