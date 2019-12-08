0673888aa2ff8ad40a263b7cbe3ed2fdcdcae0c2
BeYkeRYkt <beykerykt@gmail.com>
2019-12-09 02:31:42 +0900

    nx563j: drop qcom adaptive display
    * It is no longer available on stock
    
    Change-Id: Ia36b4813e08dc85bbb58b3e51c4e679b9da25610

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -16,10 +16,7 @@
 
 # Cabl
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qualcomm.cabl=2 \
-    ro.qcom.ad=1 \
-    ro.qcom.ad.calib.data=/vendor/etc/calib.cfg \
-    ro.qcom.ad.sensortype=2
+    ro.qualcomm.cabl=2
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
