f8b64fe52b49eaff53fb29b102966794e723c28f
BeYkeRYkt <beykerykt@gmail.com>
2020-01-27 18:59:19 +0900

    nx563j: drop input idc
    * No longer available in stock
    
    Change-Id: I74a4e506ab7cd6e2b9da6a3c733ce0efefd3621c

--- device.mk
+++ device.mk
@@ -64,10 +64,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/display/qdcm_calib_data_jdi_r63452_1080p_5p5_mipi_cmd_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_jdi_r63452_1080p_5p5_mipi_cmd_panel.xml
 
-# Input
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/idc/input_proxy.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/input_proxy.idc
-
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/nubia_synaptics_dsx.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/nubia_synaptics_dsx.kl
