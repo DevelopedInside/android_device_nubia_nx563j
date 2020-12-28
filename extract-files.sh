9aff60625d0ac52078d72349bc6f79dbb29672c4
Michael Bestas <mkbestas@lineageos.org>
2020-12-28 20:43:12 +0200

    nx563j: Switch to standalone extract utils
    Align with templates while we are at it
    
    Change-Id: I3cd279191c7bd3582f1c1f80c588eb26cf9da933

--- extract-files.sh
+++ extract-files.sh
@@ -1,40 +1,30 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 function blob_fixup() {
     case "${1}" in
-    # Patch blobs for VNDK
-    vendor/lib/hw/camera.msm8998.so)
-        sed -i "s|libgui.so|libfui.so|g" "${2}"
-        "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
-        ;;
-    vendor/lib/libnubia_effect.so | vendor/lib64/libnubia_effect.so)
-        sed -i "s|libgui.so|libfui.so|g" "${2}"
-        ;;
-    vendor/lib/libNubiaImageAlgorithm.so)
-        "${PATCHELF}" --remove-needed  "libjnigraphics.so" "${2}"
-        "${PATCHELF}" --remove-needed  "libnativehelper.so" "${2}"
-        "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
-        "${PATCHELF}" --add-needed "libNubiaImageAlgorithm_shim.so" "${2}"
-        ;;
-    vendor/lib/libarcsoft_picauto.so)
-        "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
-        ;;
+        # Patch blobs for VNDK
+        vendor/lib/hw/camera.msm8998.so)
+            sed -i "s|libgui.so|libfui.so|g" "${2}"
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
+            ;;
+        vendor/lib/libnubia_effect.so | vendor/lib64/libnubia_effect.so)
+            sed -i "s|libgui.so|libfui.so|g" "${2}"
+            ;;
+        vendor/lib/libNubiaImageAlgorithm.so)
+            "${PATCHELF}" --remove-needed  "libjnigraphics.so" "${2}"
+            "${PATCHELF}" --remove-needed  "libnativehelper.so" "${2}"
+            "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
+            "${PATCHELF}" --add-needed "libNubiaImageAlgorithm_shim.so" "${2}"
+            ;;
+        vendor/lib/libarcsoft_picauto.so)
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
+            ;;
     esac
 }
 
